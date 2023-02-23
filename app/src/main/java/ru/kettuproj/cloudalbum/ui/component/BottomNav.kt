83ddaaf4cf6c8add728a1248eb54ea63ad5c5a90
QwertyMo <qwertymo26@gmail.com>
2023-02-23 11:22:30 +0300

    Improve navigation

--- app/src/main/java/ru/kettuproj/cloudalbum/ui/component/BottomNav.kt
+++ app/src/main/java/ru/kettuproj/cloudalbum/ui/component/BottomNav.kt
@@ -1,5 +1,9 @@
 package ru.kettuproj.cloudalbum.ui.component
 
+import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
@@ -11,7 +15,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
-import ru.kettuproj.cloudalbum.model.BottomNavItem
+import ru.kettuproj.cloudalbum.R
 import ru.kettuproj.cloudalbum.screen.Destination
 import ru.kettuproj.cloudalbum.screen.navigate
 
@@ -25,30 +29,54 @@ fun BottomNav(
         BottomNavItem.Albums,
     )
 
-    BottomNavigation {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentDestination = navBackStackEntry?.destination
+    val hideOn = listOf(
+        Destination.LOGIN,
+        Destination.SPLASH,
+        Destination.IMAGE
+    )
 
-        var selectedItem by remember { mutableStateOf(Destination.USER_IMAGES.dest) }
-        if(currentDestination?.route!=null)
-            if(items.map { it.route.dest }.contains(currentDestination.route))
-                selectedItem = currentDestination.route!!
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    var selectedItem by remember { mutableStateOf(Destination.USER_IMAGES.dest) }
+    val route = currentDestination?.route?.split("/")?.get(0) ?: ""
 
-        items.forEach { screen ->
-            BottomNavigationItem(
-                icon = { Icon(painterResource(screen.icon), contentDescription = null) },
-                label = { Text(stringResource(screen.resourceId)) },
-                selected = currentDestination?.hierarchy?.any { screen.route.dest == selectedItem } == true,
-                onClick = {
-                    navController.navigate(screen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
+    var hided by remember { mutableStateOf(true) }
+    hided = !hideOn.map { it.dest }.contains(route)
+
+    AnimatedVisibility(
+        visible = hided,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        BottomNavigation {
+            if(items.map { it.route.dest }.contains(route))
+                selectedItem = route
+
+            items.forEach { screen ->
+                BottomNavigationItem(
+                    icon = { Icon(painterResource(screen.icon), contentDescription = null) },
+                    label = { Text(stringResource(screen.resourceId)) },
+                    selected = currentDestination?.hierarchy?.any { screen.route.dest == selectedItem } == true,
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
                     }
-                }
-            )
+                )
+            }
         }
     }
+}
+
+sealed class BottomNavItem(
+    val route:      Destination,
+    val icon:       Int,
+    @StringRes val resourceId: Int){
+
+    object Profile : BottomNavItem(Destination.USER_IMAGES, R.drawable.ic_launcher_foreground, R.string.profile)
+    object Albums  : BottomNavItem(Destination.ALBUMS, R.drawable.ic_launcher_foreground, R.string.albums)
 }
